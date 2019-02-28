c69faa59b350134c3a8333c3361aadc6df470f87
Isaac Morneau <isaacmorneau@gmail.com>
2019-02-28 00:22:38 -0800

    basic options

--- src/main.c
+++ src/main.c
@@ -1,68 +1,77 @@
 #include <getopt.h>
+#include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+size_t total_interfaces = 0;
+char **interfaces       = NULL;
+
+static void print_help(void) {
+    puts(
+        "joymon - a simple daemon to respond to joystick events\n"
+        "-[-i]nterface - the /dev/input/js# device to read from. can be specificed multiple times\n"
+        "-[-c]onfig - defaults to XDG_CONFIG_HOME/joymon/config\n"
+        "-[-h]elp - this message");
+}
+
 int main(int argc, char **argv) {
+    if (argc == 1) {
+        print_help();
+        exit(EXIT_SUCCESS);
+    }
+
     int c;
-    int digit_optind = 0;
 
     while (1) {
-        int this_option_optind              = optind ? optind : 1;
-        int option_index                    = 0;
-        static struct option long_options[] = {{"add", required_argument, 0, 0},
-            {"append", no_argument, 0, 0}, {"delete", required_argument, 0, 0},
-            {"verbose", no_argument, 0, 0}, {"create", required_argument, 0, 'c'},
-            {"file", required_argument, 0, 0}, {0, 0, 0, 0}};
+        int option_index = 0;
 
-        c = getopt_long(argc, argv, "abc:d:012", long_options, &option_index);
+        static struct option long_options[] = {{"config", required_argument, 0, 'c'},
+            {"interface", required_argument, 0, 'i'}, {"help", no_argument, 0, 'h'}, {0, 0, 0, 0}};
+
+        c = getopt_long(argc, argv, "i:c:h", long_options, &option_index);
         if (c == -1)
             break;
 
         switch (c) {
-            case 0:
-                printf("option %s", long_options[option_index].name);
-                if (optarg)
-                    printf(" with arg %s", optarg);
-                printf("\n");
-                break;
-
-            case '0':
-            case '1':
-            case '2':
-                if (digit_optind != 0 && digit_optind != this_option_optind)
-                    printf("digits occur in two different argv-elements.\n");
-                digit_optind = this_option_optind;
-                printf("option %c\n", c);
-                break;
-
-            case 'a':
-                printf("option a\n");
-                break;
-
-            case 'b':
-                printf("option b\n");
-                break;
-
             case 'c':
-                printf("option c with value '%s'\n", optarg);
+                printf("config not yet implemented\n");
+                exit(EXIT_FAILURE);
+                break;
+            case 'i':
+                ++total_interfaces;
+
+                if (interfaces == NULL && !(interfaces = malloc(sizeof(char *)))) {
+                    perror("malloc");
+                    exit(EXIT_FAILURE);
+                } else {
+                    if (!(interfaces = realloc(interfaces, sizeof(char *) * total_interfaces))) {
+                        perror("malloc");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+
+                interfaces[total_interfaces - 1] = optarg;
+
+                printf("using interface '%s'\n", optarg);
                 break;
 
-            case 'd':
-                printf("option d with value '%s'\n", optarg);
-                break;
-
+            case 'h':
             case '?':
+                print_help();
                 break;
-
-            default:
-                printf("?? getopt returned character code 0%o ??\n", c);
         }
     }
 
+    //while inform people its not being used
     if (optind < argc) {
-        printf("non-option ARGV-elements: ");
+        printf("ignoring args: ");
         while (optind < argc) printf("%s ", argv[optind++]);
         printf("\n");
+    }
+
+    for (size_t i = 0; i < total_interfaces; ++i) {
+        printf("listening to: %s\n", interfaces[i]);
     }
 
     exit(EXIT_SUCCESS);
